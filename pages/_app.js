f505892ba530dd1d9e196766f174952dfa55d265
GoRuGoo <dachanyr8989@gmail.com>
2023-01-11 23:49:18 +0900

    feat: 簡単なヘッダーの表示に成功

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Box, ChakraProvider } from "@chakra-ui/react";
+import { Children } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
+import Layout from "../components/layouts/Layout";
 import Head from "next/head";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title>GoRuGoo Portfolio</title>
+        <title>GoRuGoo&apos;s Portfolio</title>
         <meta name="description" content="GoRuGoo's Portfolio" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main></main>
+      <Layout />
     </>
   );
 }
