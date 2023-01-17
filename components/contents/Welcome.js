684f247c4150ad7bb2c77220df33f3215f86155c
GoRuGoo <dachanyr8989@gmail.com>
2023-01-17 22:16:20 +0900

    test: 一応安定して動くっぽい

--- components/contents/Welcome.js
+++ components/contents/Welcome.js
@@ -2,9 +2,15 @@ import Image from "next/legacy/image";
 export default function Welcome(props) {
   return (
     <>
-      <div>
+      <div className="bg-white">
         {/* <Image src="/cycle.webp" height={2048} width={1365} /> */}
-        <Image src={"/cycle.webp"} layout="fill" />
+        <Image
+          src={"/cycle.webp"}
+          height={props.height}
+          width={props.width}
+          objectPosition="left top"
+          className="object-cover"
+        />
       </div>
     </>
   );
