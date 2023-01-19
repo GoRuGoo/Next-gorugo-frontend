6d67dde167ef0cbb047b5da4c7e8355bc623ab6a
GoRuGoo <dachanyr8989@gmail.com>
2023-01-19 18:10:05 +0900

    wip: 紹介文が追加、画像サイズ取得関数編集のため一度commit

--- components/firstpage/Biography.js
+++ components/firstpage/Biography.js
@@ -1,21 +1,36 @@
 export default function Biography() {
   return (
-    <div className="inline w-fit">
-      <h1 className="font-bold  text-5xl  border-black ">BIOGRAPHY</h1>
+    // <div className="inline w-fit">
+    <div className="inline item-center">
+      <div className="wx-6/12 mx-auto text-center my-10">
+        <h1
+          className="font-bold  text-5xl  border-black border-l-4
+      border-r-4 px-3 mx-auto  wx-6/12 inline-block"
+        >
+          BIOGRAPHY
+        </h1>
+      </div>
+      <div></div>
       <h1 className="text-center font-namemyotyo text-6xl">伊藤 優汰</h1>
-      <h2>Yuta Ito</h2>
+      <h2 className="text-center font-namemyotyo text-3xl mt-3 mb-3">
+        Yuta Ito
+      </h2>
       <div>
-        <p className=" w-9/12 ml-auto mr-auto text-center">
-          2005年生まれ。現17歳。
-          <br />
-          名古屋市在住。
-          <br />
-          小5〜中3の間RoboCup Junior Soccerに参加。
+        <p className=" w-6/12 mx-auto text-center">
+          2005年生まれ。現17歳。 名古屋市在住。
+          <br /> 小5〜中3の間RoboCup Junior Soccerに参加。
           <br />
           それをきっかけに科学技術に興味を持つ。
+          現在はWeb技術に関するプログラミングを積極的に行っている。
           <br />
-          現在はWebに関するプログラミングに興味を持っています。
+          豊田工業高等専門学校
+          情報工学科所属、授業では情報から回路まで、幅広く学んでいる。
+          <br />
+          プログラミングとVTuberの
+          <a href="https://www.youtube.com/@HoshimachiSuisei">星街すいせい</a>
+          を愛しています。
         </p>
+        <span></span>
       </div>
     </div>
   );
