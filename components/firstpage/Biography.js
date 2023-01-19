334ad81fa5ec7148c59c2d2f655ba5b22807aae8
GoRuGoo <dachanyr8989@gmail.com>
2023-01-19 13:48:04 +0900

    feat: テキストを真ん中に配置

--- components/firstpage/Biography.js
+++ components/firstpage/Biography.js
@@ -3,6 +3,20 @@ export default function Biography() {
     <div className="inline w-fit">
       <h1 className="font-bold  text-5xl  border-black ">BIOGRAPHY</h1>
       <h1 className="text-center font-namemyotyo text-6xl">伊藤 優汰</h1>
+      <h2>Yuta Ito</h2>
+      <div>
+        <p className=" w-9/12 ml-auto mr-auto text-center">
+          2005年生まれ。現17歳。
+          <br />
+          名古屋市在住。
+          <br />
+          小5〜中3の間RoboCup Junior Soccerに参加。
+          <br />
+          それをきっかけに科学技術に興味を持つ。
+          <br />
+          現在はWebに関するプログラミングに興味を持っています。
+        </p>
+      </div>
     </div>
   );
 }
