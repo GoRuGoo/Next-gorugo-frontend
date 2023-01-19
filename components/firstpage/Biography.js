65fb3a26f70359812aad6caf92feea4cfae5a66b
GoRuGoo <dachanyr8989@gmail.com>
2023-01-19 13:16:53 +0900

    feat: 中央に配置できたうれしい

---
+++ components/firstpage/Biography.js
@@ -0,0 +1,8 @@
+export default function Biography() {
+  return (
+    <div className="inline w-fit">
+      <h1 className="font-bold  text-5xl  border-black ">BIOGRAPHY</h1>
+      <h1 className="text-center font-namemyotyo text-6xl">伊藤 優汰</h1>
+    </div>
+  );
+}
