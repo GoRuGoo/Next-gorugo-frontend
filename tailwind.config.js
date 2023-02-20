0941d37e6ba218098b73caada57451fcb26a76c9
GoRuGoo <dachanyr8989@gmail.com>
2023-02-20 23:45:23 +0900

    feat: TypeScriptにTailwindを対応させた

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  content: ["./pages/**/*.{html,tsx}", "./components/**/*.{html,tsx}"],
   theme: {
     extend: {
       fontFamily: {
